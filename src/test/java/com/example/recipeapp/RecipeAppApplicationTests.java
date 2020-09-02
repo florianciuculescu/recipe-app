531b2f56f73ca9f9936ed124896caac45b25c2d1
fciuculescu <fciuculescu@deloittece.com>
2020-09-02 14:19:55 +0300

    Project Setup

---
+++ src/test/java/com/example/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
