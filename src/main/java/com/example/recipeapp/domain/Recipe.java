9ff16373db43b05cefd126f93fbc103c557ad16c
fciuculescu <fciuculescu@deloittece.com>
2020-09-03 19:19:45 +0300

    Created Ingredient Entity and implemented 1toMany relathionship with Recipe

--- src/main/java/com/example/recipeapp/domain/Recipe.java
+++ src/main/java/com/example/recipeapp/domain/Recipe.java
@@ -6,7 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -23,6 +25,9 @@ public class Recipe {
     private String url;
     private String directions;
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -92,6 +97,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
