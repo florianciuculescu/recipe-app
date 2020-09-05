a058091d7f1550930767dff0e02b3dbb85beb417
fciuculescu <fciuculescu@deloittece.com>
2020-09-05 17:23:28 +0300

    Added Lombok to pom.xml

--- src/main/java/com/example/recipeapp/domain/Notes.java
+++ src/main/java/com/example/recipeapp/domain/Notes.java
@@ -1,5 +1,7 @@
 package com.example.recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
+@Data
 @Entity
 public class Notes {
 
@@ -20,27 +23,11 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
-    public Long getId() {
-        return id;
+    public Notes() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    protected boolean canEqual(final Object other) {
+        return other instanceof Notes;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
