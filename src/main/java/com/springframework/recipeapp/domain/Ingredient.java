65ebcd3e04a1dc50e15d08e97b0f0da1929d7727
Florian Ciuculescu <Florian.Ciuculescu@endava.com>
2021-09-20 15:09:19 +0300

    made one to many relationship

---
+++ src/main/java/com/springframework/recipeapp/domain/Ingredient.java
@@ -0,0 +1,52 @@
+package com.springframework.recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    /**
+     * if we delete an ingredient we don't want to delete the Recipe
+     */
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
--- src/main/java/com/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/com/springframework/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.springframework.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    /**
+     * if we delete a Recipe, it will delete all the related ingredients
+     */
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -88,6 +95,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
