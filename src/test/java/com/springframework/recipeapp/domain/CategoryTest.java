8201be0afe4ef73e286c4b189617789095719576
florian ciuculescu <florianciuculescu@yahoo.com>
2021-10-05 11:07:03 +0300

    Created a JUnit test for demo purposes

---
+++ src/test/java/com/springframework/recipeapp/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package com.springframework.recipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
